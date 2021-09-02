ca0798fa90c3c9932d5052120efcb5afbbe4086e
Shashi Ranjan <shashiranjankv@gmail.com>
2021-09-02 12:51:28 +0530

    python2->python3

--- runserver.py
+++ runserver.py
@@ -14,10 +14,10 @@ def main():
     opts, args = parser.parse_args()
     if args:
         httpd = BaseHTTPServer.HTTPServer((args[0], int(args[1])), Handler)
-        print "Collectd-web server running at http://%s:%s/" % (args[0], args[1])
+        print("Collectd-web server running at http://%s:%s/" % (args[0], args[1]))
     else:
         httpd = BaseHTTPServer.HTTPServer(("127.0.0.1", PORT), Handler)
-        print "Collectd-web server running at http://%s:%s/" % ("127.0.0.1", PORT)
+        print("Collectd-web server running at http://%s:%s/" % ("127.0.0.1", PORT))
     httpd.serve_forever()
 
 if __name__ == "__main__":
